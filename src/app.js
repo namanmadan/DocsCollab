ca4660bdab047f3f0793a3945aa6fa588fb9f66d
Naman Madan <naman.madan25@gmail.com>
2021-06-27 02:27:53 +0530

    Added functionality for MongoDB

--- src/app.js
+++ src/app.js
@@ -1,10 +1,30 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const mongoose = require("mongoose");
 
 const port = "1212";
-const documents = {};
+let docIds = [];
 
+//Connecting with mongoDb
+mongoose.connect('mongodb://localhost:27017/DocsCollab', {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log("Connected to DB."))
+.catch((err) => console.log(err));
+
+const DocumentSchema = new mongoose.Schema({
+    id: String,
+    doc: String
+}, {collection: "Document"});
+
+const documentModel = mongoose.model('Document', DocumentSchema);
+
+documentModel.find()
+.then((data) => {
+    docIds = data.map(function(obj){return obj.id});
+})
+.catch((err) => console.log(err));
+
+//Creating Socket
 io.on("connection", socket => {
     let prevId;
 
@@ -16,27 +36,46 @@ io.on("connection", socket => {
 
     socket.on("getDoc", currId => {
         safeJoin(currId);
-        socket.emit("document", documents[currId]);
+        documentModel.find({id: currId})
+        .then(data => {
+            if (data != null && data.length != 0){
+                console.log(data);
+                socket.emit("document", data[0]);
+            }        
+        })
+        
     });
 
     socket.on("addDoc", doc => {
-        documents[doc.id] = doc;
+        insertInMongoDb(doc);
+        docIds.push(doc.id);
         safeJoin(doc.id);
-        io.emit("documents", Object.keys(documents));
+        io.emit("documents", docIds);
         socket.emit("document", doc);
     });
 
     socket.on("editDoc", doc => {
-        documents[doc.id] = doc;
+        updateMongoDb(doc);
         socket.to(doc.id).emit("document", doc);
-        console.log(doc);
     });
 
-    io.emit("documents", Object.keys(documents));
+    io.emit("documents", docIds);
 
     console.log(`Socket ${socket.id} connected the server`);
 });
 
+function insertInMongoDb(doc){
+    const docModel = documentModel({id: doc.id, data: doc.data});
+    docModel.save()
+    .catch((err) => console.log(err));;
+}
+
+function updateMongoDb(doc){
+    console.log(doc);
+    documentModel.updateOne({id: doc.id}, doc)
+    .catch((err) => console.log(err));;
+}
+
 http.listen(port, ()=>{
     console.log(`Server Listening at ${port} port`);
-})
+});
