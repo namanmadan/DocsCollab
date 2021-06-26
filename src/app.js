524bdcae8b4d5879ae4beb42214c744fa92be99d
Naman Madan <naman.madan25@gmail.com>
2021-06-26 23:28:41 +0530

    Created Socket Server

---
+++ src/app.js
@@ -0,0 +1,41 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const port = "1212";
+const documents = {};
+
+io.on("connection", socket => {
+    let prevId;
+
+    const safeJoin = currId => {
+        socket.leave(prevId);
+        socket.join(currId, () => {console.log(`Socket ${socket.id} joind room ${currId}`)});
+        prevId = currId;
+    };
+
+    socket.on("getDoc", currId => {
+        safeJoin(currId);
+        socket.emit("document", documents[currId]);
+    });
+
+    socket.on("addDoc", doc => {
+        documents[doc.id] = doc;
+        safeJoin(doc.id);
+        io.emit("documents", Object.keys(documents));
+        socket.emit("document", doc);
+    });
+
+    socket.on("editDoc", doc => {
+        documents[doc.id] = doc;
+        socket.to(doc.id).emit("document", doc);
+    });
+
+    io.emit("documents", Object.keys(documents));
+
+    console.log(`Socket ${socket.id} connected the server`);
+});
+
+http.listen(port, ()=>{
+    console.log(`Server Listening at ${port} port`)
+})
